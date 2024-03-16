e322369adc4437d1cf4b4158b874dbacc6defeb6
shujashah <shuja.ashraf11@gmail.com>
2024-03-16 16:59:09 +0530

    added the input component

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,10 @@
-import Input from '@/components/Input';
+import { useState } from 'react';
+import Input from '../components/Input';
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="relative h-full  w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg: bg-opacity-50">
@@ -10,8 +15,29 @@ const Auth = () => {
               <h2 className="text-white text-4xl mb-8 font-semibold">
                 Sign in
               </h2>
-              <div className="flex flex-col-4 gap-4">
-                <Input />
+              <div className="flex flex-col gap-4">
+                <Input
+                  label="Username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  type="text"
+                  value={name}
+                />
+                <Input
+                  label="Email"
+                  onChange={(e: any) => setEmail(e.target.value)}
+                  id="email"
+                  type="email"
+                  value={email}
+                />
+
+                <Input
+                  label="Password"
+                  onChange={(e: any) => setPassword(e.target.value)}
+                  id="password"
+                  type="password"
+                  value={password}
+                />
               </div>
             </div>
           </div>
