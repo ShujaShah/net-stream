54cf462e3a096be0d31a7f23f341bf52f9e56018
Shuja Shah <shuja.ashraf11@gmail.com>
2024-03-17 23:44:18 +0530

    Update prismadb.ts

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,24 +1,24 @@
-// import { PrismaClient } from '@prisma/client';
-
-// const client = global.prismadb || new PrismaClient();
-// if (process.env.NODE_ENV !== 'production') global.prismadb = client;
-
-// export default client;
-
 import { PrismaClient } from '@prisma/client';
 
-let client: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  client = new PrismaClient();
-} else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
-  }
-  client = globalWithPrisma.prisma;
-}
+const client = global.prismadb || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') global.prismadb = client;
 
 export default client;
+
+// import { PrismaClient } from '@prisma/client';
+
+// let client: PrismaClient;
+
+// if (process.env.NODE_ENV === 'production') {
+//   client = new PrismaClient();
+// } else {
+//   let globalWithPrisma = global as typeof globalThis & {
+//     prisma: PrismaClient;
+//   };
+//   if (!globalWithPrisma.prisma) {
+//     globalWithPrisma.prisma = new PrismaClient();
+//   }
+//   client = globalWithPrisma.prisma;
+// }
+
+// export default client;
