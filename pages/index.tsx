e72ba91439fb073f9c0dea22cfc6c88dbda22c26
shujashah <shuja.ashraf11@gmail.com>
2024-03-17 18:07:50 +0530

    implemented profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+import { NextPageContext } from 'next';
+import { getSession, signOut } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
-const inter = Inter({ subsets: ['latin'] });
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <h1 className="text-2xl text-green-500">Netflix</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
